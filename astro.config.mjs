101c87e178a83ed2fe3ea209d09810573d5971d5
dream2023 <1098626505@qq.com>
2023-09-20 18:00:21 +0800

    fix: blog cn

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.nocobase.com/", // replace this with your deployed domain
+  site: "https://blog-cn.nocobase.com/", // replace this with your deployed domain
   integrations: [
     tailwind({
       config: {
